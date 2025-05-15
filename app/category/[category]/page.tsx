0cc40c8eaa9adceb244f4cad4afa18f4941795f0
Challmejosh <challmejosh@gmail.com>
2025-05-15 07:52:44 -0700

    more feature coming soon

--- app/category/[category]/page.tsx
+++ app/category/[category]/page.tsx
@@ -1,10 +1,14 @@
-
 "use client";
-import dynamic from 'next/dynamic';
 
-const Product = dynamic(() => import('@/component/categoryproduct'), { ssr: false });
+import Product from '@/component/categoryproduct';
+// import dynamic from 'next/dynamic';
+import { useParams } from 'next/navigation';
 
-const Page = ({ params }: { params: { category: string } }) => {
+
+// const Product = dynamic(() => import('@/component/categoryproduct'), { ssr: false });
+
+const Page = () => {
+    const params = useParams()
   return <Product text={params.category} />;
 };
 
